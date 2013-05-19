34cc42f0213100134068ac6dc2cbdbb288304fe9
Adam Bronfin <adam_bronfin@yahoo.com>
2013-05-19 00:24:15 -0700

    Set up basic layout

--- Songs by sinatra/songs.rb
+++ Songs by sinatra/songs.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
+require 'slim'
+require 'sass' 
+
+set :public_folder, 'assets'
+get('/styles.css') { scss :styles.to_sym }
 
 get '/' do
-  erb :home
+  slim :home 
 end
 
+get '/about' do 
+  @name = "adam"
+  @title = "This is your about page! This website is all about Frank Sinatra and his ultra smoothness!"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
